a62727a4373a275c923669b4366f4504adbfb770
Mikhailrs <msghost771@gmail.com>
2022-10-01 04:38:32 +0300

    Filled session_controller, add page for session, add home page, add routes for session

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,23 @@
 class SessionsController < ApplicationController
+
+  def new
+  end
+
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+
+    if user && user.authenticate(params[:session][:password])
+      session[:user_id] = user.id
+      redirect_to user_path(user), notice: 'Вы вошли на сайт'
+    else
+      flash.now[:alert] = 'Неправильный email или пароль'
+      render 'new'
+    end
+  end
+
+  def destroy
+    session.delete(:user_id)
+
+    redirect_to root_path, notice: 'Вы вышли из аккаунта'
+  end
 end
