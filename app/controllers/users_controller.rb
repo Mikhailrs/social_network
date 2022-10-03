bb5fdcab7137bc96a65337d7b54fa76c4d6daaae
Mikhailrs <msghost771@gmail.com>
2022-10-03 17:46:45 +0300

    Add separation of access rights and admin column

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, only: [:index, :edit, :update]
+  before_action :correct_user, only: [:edit, :update]
 
   def index
     @users = User.paginate(page: params[:page], per_page: 15)
@@ -23,11 +25,9 @@ class UsersController < ApplicationController
   end
 
   def edit
-    @user = User.find(params[:id])
   end
 
   def update
-    @user = User.find(params[:id])
     @user.update(user_params)
     if @user.save
       redirect_to user_path(@user), notice: 'Аккаунт изменен'
@@ -47,5 +47,16 @@ class UsersController < ApplicationController
 
   def user_params
     params.require(:user).permit(:name, :email, :password, :password_confirmation, :avatar)
+  end
+
+  def logged_in_user
+    unless logged_in?
+      redirect_to login_path, alert: 'Войдите в аккаунт'
+    end
+  end
+
+  def correct_user
+    @user = User.find_by(id: params[:id])
+    redirect_to root_path unless @user == current_user
   end
 end
