2d55906d2be73454ec6116c4bd86a0ba7fba1225
Mikhailrs <msghost771@gmail.com>
2022-10-07 16:10:19 +0300

    Fix current_user, validates for email and add .idea in gitignore

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,13 +1,15 @@
 module SessionsHelper
 
   def current_user
-    if session[:user_id]
-      @current_user ||= User.find_by(id: session[:user_id])
-    elsif cookies.signed[:user_id]
-      user = User.find_by(id: cookies.signed[:user_id])
-      if user && user.authenticated?(cookies[:remember_token])
-        log_in user
-        @current_user = user
+    @current_user ||= begin
+      if session[:user_id]
+        User.find_by(id: session[:user_id])
+      elsif cookies.signed[:user_id]
+        user = User.find_by(id: cookies.signed[:user_id])
+        if user && user.authenticated?(cookies[:remember_token])
+          log_in user
+          user
+        end
       end
     end
   end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,22 +1,25 @@
 class User < ApplicationRecord
   attr_accessor :remember_token
+
   has_one_attached :avatar
+
   validates :avatar, content_type: ['image/png', 'image/jpeg'],
             size: {less_than: 1.megabyte}
   validates :name, presence: true, length: {maximum: 50}
-  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)+\z/i
-  validates :email, presence: true, format: {with: VALID_EMAIL_REGEX}, length: {maximum: 255}
+  validates :email, presence: true, format: {with: URI::MailTo::EMAIL_REGEXP}, length: {maximum: 255}
   validates :password, presence: true, confirmation: true, length: {minimum: 6}, allow_nil: true
   validates :password_confirmation, presence: true, allow_blank: true
+
   before_save { self.email = email.downcase }
+
   has_secure_password
 
-  def User.digest(string)
+  def self.digest(string)
     cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST : BCrypt::Engine.cost
     BCrypt::Password.create(string, cost: cost)
   end
 
-  def User.new_token
+  def self.new_token
     SecureRandom.urlsafe_base64
   end
 
